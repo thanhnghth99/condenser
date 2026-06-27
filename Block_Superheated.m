982a7e8a9a9151f40e7e1199cf083cb00219fcbf
thanhnghth99 <thanhnghth99@gmail.com>
2026-06-28 00:42:02 +0900

    Superheated + dP_entrance

--- Block_Superheated.m
+++ Block_Superheated.m
@@ -1,16 +1,18 @@
-function [L_sh, P_out_sh, dP_sh, T_sat_v, h_sat_v, Q_sh] = Block_Superheated(P_ref_in, T_ref_in, m_ref, T_air_in, m_air_in, h_air, D_h, alpha, W_cond, A_tube_total, A_surface_total, eta_o, G)
+function [L_sh, P_out_sh, dP_sh, dP_entrance, T_sat_v, h_sat_v, Q_sh] = Block_Superheated(P_ref_in, T_ref_in, m_ref, T_air_in, m_air_in, h_air, D_h, alpha, W_cond, A_tube_total, A_surface_total, eta_o, G, sigma)
     % coder.extrinsic('py.CoolProp.CoolProp.PropsSI');
     Refrig = 'R134a';
     
     % CoolProp library
     h_sat_v = py.CoolProp.CoolProp.PropsSI('H', 'P', P_ref_in, 'Q', 1, Refrig);
     h_ref_in = py.CoolProp.CoolProp.PropsSI('H', 'P', P_ref_in, 'T', T_ref_in, Refrig);
-    cp_v = py.CoolProp.CoolProp.PropsSI('C', 'P', P_ref_in, 'T', T_ref_in, Refrig);
-    mu_v = py.CoolProp.CoolProp.PropsSI('V', 'P', P_ref_in, 'T', T_ref_in, Refrig);
-    k_v = py.CoolProp.CoolProp.PropsSI('L', 'P', P_ref_in, 'T', T_ref_in, Refrig);
-    rho_v = py.CoolProp.CoolProp.PropsSI('D', 'P', P_ref_in, 'T', T_ref_in, Refrig);
-    Pr_v = py.CoolProp.CoolProp.PropsSI('PRANDTL', 'P', P_ref_in, 'T', T_ref_in, Refrig);
     T_sat_v = py.CoolProp.CoolProp.PropsSI('T', 'P', P_ref_in, 'Q', 1, Refrig);
+
+    T_rm_sh = (T_ref_in + T_sat_v) / 2;
+    cp_v = py.CoolProp.CoolProp.PropsSI('C', 'P', P_ref_in, 'T', T_rm_sh, Refrig);
+    mu_v = py.CoolProp.CoolProp.PropsSI('V', 'P', P_ref_in, 'T', T_rm_sh, Refrig);
+    k_v = py.CoolProp.CoolProp.PropsSI('L', 'P', P_ref_in, 'T', T_rm_sh, Refrig);
+    rho_v = py.CoolProp.CoolProp.PropsSI('D', 'P', P_ref_in, 'T', T_rm_sh, Refrig);
+    Pr_v = py.CoolProp.CoolProp.PropsSI('PRANDTL', 'P', P_ref_in, 'T', T_rm_sh, Refrig);
 
     % 1. Required rejected heat transfer rate to saturated vapor (Q_req)
     Q_req = m_ref * (h_ref_in - h_sat_v);
@@ -78,14 +80,73 @@ function [L_sh, P_out_sh, dP_sh, T_sat_v, h_sat_v, Q_sh] = Block_Superheated(P_r
     Q_sh = Q_eNTU_n;
 
     % 4. Calculate outlet pressure and pressure drop
-    Re_Dh = G * D_h / mu_v;
+    dP_sh = PressureDrop_SH(L_sh, rho_v, mu_v, D_h, alpha, G);
+    
+    % 5. Calculate minor losses at entrance
+    dP_entrance = PressureDrop_entrance(P_ref_in, T_ref_in, Refrig, D_h, G, sigma);
+
+    % 6. Total pressure drop of superheated region
+    P_out_sh = P_ref_in - dP_sh - dP_entrance;
+end
+
+function dP_entrance = PressureDrop_entrance(P_ref_in, T_ref_in, Refrig, D_h, G, sigma)
+    rho_in = py.CoolProp.CoolProp.PropsSI('D', 'P', P_ref_in, 'T', T_ref_in, Refrig);
+    mu_in = py.CoolProp.CoolProp.PropsSI('V', 'P', P_ref_in, 'T', T_ref_in, Refrig);
+    
+    Re_Dh = G * D_h / mu_in;
+
     if Re_Dh < 2300
-        f_D = 4 * (24 / Re_Dh) * (1 - 1.3553*alpha + 1.9467*alpha^2 - 1.7012*alpha^3 + 0.9564*alpha^4 - 0.2537*alpha^5);
+        % Entrance pressure loss coefficients Kc for  multiple-tube flat-tube core From Kays and London, 1998
+        sigma_lami_array = [0.017172, 0.063467, 0.12194, 0.17791, 0.2363, 0.30077, 0.35295, 0.40883, 0.46953, 0.51315, 0.54591, 0.581, 0.6149, 0.66586, 0.70447, 0.73717, 0.76859, 0.79638, 0.82541, 0.86284, 0.89911, 0.93416, 0.95944, 0.99205, 1.0077];
+        Kc_lami_array = [0.79657, 0.79621, 0.79576, 0.78974, 0.78183, 0.77387, 0.75668, 0.7432, 0.72595, 0.70696, 0.69552, 0.6766, 0.65955, 0.64237, 0.61223, 0.59706, 0.5763, 0.55744, 0.54043, 0.51403, 0.49137, 0.46872, 0.44428, 0.42165, 0.40661];
+        
+        % Interpolation for Kc corresponding sigma
+        Kc_interp = interp1(sigma_lami_array, Kc_lami_array, sigma, 'linear');
     else
-        f_D = 1 / (0.79 * log(Re_Dh) - 1.64)^2;
+        sigma_turb_array = [0.0092614, 0.043363, 0.085993, 0.11889, 0.16392, 0.21384, 0.2455, 0.28929, 0.33433, 0.37686, 0.4231, 0.46929, 0.49722, 0.53856, 0.57622, 0.60415, 0.64302, 0.67945, 0.71468, 0.74742, 0.78137, 0.81418, 0.85904, 0.90151, 0.93426, 0.95366, 0.98156, 1.0034];
+        Kc_turb_array = [0.46548, 0.46337, 0.46119, 0.46094, 0.4532, 0.44727, 0.44518, 0.43375, 0.42785, 0.40903, 0.39943, 0.38243, 0.36742, 0.35416, 0.33722, 0.32221, 0.30342, 0.28464, 0.26773, 0.24343, 0.21728, 0.20408, 0.16859, 0.14237, 0.11808, 0.10313, 0.084422, 0.065758];
+
+        % Interpolation for Kc corresponding sigma
+        Kc_interp = interp1(sigma_turb_array, Kc_turb_array, sigma, 'linear');
     end
+
+    disp(Kc_interp);
+
+    % Pressure drop due to minor losses
+    dP_entrance = (G^2 / (2*rho_in)) * (1 - sigma^2 + Kc_interp);
+end
+
+% Pressure drop function
+function dP_sh = PressureDrop_SH(L_sh, rho_v, mu_v, D_h, alpha, G)
+    if L_sh <= 1e-6
+        dP_sh = 0;
+        return;
+    end
+
+    Re_Dh = G * D_h / mu_v;
+
+    if Re_Dh < 2300
+        % Hydrodynamic entrance region for noncircular duct
+        x_plus = L_sh / (D_h * Re_Dh);
+        x_plus = max(x_plus, 1e-10); % Avoid division-by-zero singularity
+
+        % Fully developed Fanning friction factor
+        fRe_fd = 24 * (1 - 1.3553*alpha + 1.9467*alpha^2 - 1.7012*alpha^3 + 0.9564*alpha^4 - 0.2537*alpha^5);
+
+        % Incremental pressure drop number K(infinity)
+        K_inf = 0.6796 + 1.2197*alpha + 3.3089*alpha^2 - 9.5921*alpha^3 + 8.9089*alpha^4 - 2.9959*alpha^5;
+
+        % Apparent Fanning friction factor times Re correlation
+        fappRe = (3.44 / sqrt(x_plus)) + (fRe_fd + (K_inf / (4 * x_plus)) - (3.44 / sqrt(x_plus))) / (1 + 0.0002367 / (x_plus^2));
+
+        % Convert Apparent Fanning to Darcy friction factor (f_darcy = 4 * f_fanning)
+        f_D = 4 * (fappRe / Re_Dh);
+    else
+        % Turbulent flow (Darcy friction factor via Petukhov)
+        f_D = 1 / (0.79 * log(Re_Dh) - 1.64)^2; 
+    end
+
     dP_sh = f_D * L_sh * (G^2) / (2 * D_h * rho_v);
-    P_out_sh = P_ref_in - dP_sh;
 end
 
 % Heat transfer function using e-NTU method
@@ -109,8 +170,15 @@ function Q_eNTU = HeatTransfer_eNTU_SH(L_sh, m_ref, T_ref_in, T_air_in, m_air_in
     Re_Dh = G * D_h / mu_v;
 
     if Re_Dh < 2300
+        % Fully developed Nusselt number
+        Nu_fd = 7.541 * (1 - 2.610*alpha + 4.970*alpha^2 - 5.119*alpha^3 + 2.702*alpha^4 - 0.548*alpha^5);
+
         % Laminar flow in rectangular flat tube (Shah & London)
-        Nu_sh = 7.541 * (1 - 2.610*alpha + 4.970*alpha^2 - 5.119*alpha^3 + 2.702*alpha^4 - 0.548*alpha^5);
+        x_star = L_sh / (D_h * Re_Dh * Pr_v);
+        x_star = max(x_star, 1e-10); % Avoid singularity
+
+        % Stephan correlation for developing Nusselt number
+        Nu_sh = Nu_fd + 0.0668 / (x_star^(1/3) * (0.04 + x_star^(2/3)));
         h_sh = Nu_sh * (k_v / D_h);
     else
         % Turbulent flow (Darcy friction factor)
